2f75934ab598aaf51e6e9c94d860d0b7433594de
Edward Mack <ed@edwardmack.com>
2017-11-28 16:33:40 -0500

    error invalid opcode

--- test/contract_factory.js
+++ test/contract_factory.js
@@ -3,6 +3,7 @@ var CSAToken = artifacts.require("./CSAToken.sol");
 
 contract("CSATokenFactory", function(accounts) {
     var factory;
+    var tokenInstance;
     it("should create factory" ,function() {
         return CSATokenFactory.deployed()
         .then(function(instance) {
@@ -31,14 +32,38 @@ contract("CSATokenFactory", function(accounts) {
             return factory.getAddress.call(0);
         })
         .then(function(result) {
-            var myTokenInst = CSAToken.at(result);
-            console.log("after get info", myTokenInst.name);
-            return myTokenInst.name.call();
+            tokenInst = CSAToken.at(result);
+            console.log("after get info", tokenInst.name);
+            return tokenInst.name.call();
         })
         .then(function(result) {
             console.log("name:", result);
             assert.equal(result, "Farm Name", "Expected value Farm Name");
         });
+    });
+
+    it("should transfer token", function() {
+        var ownerAddress;
+        return factory.getAddress.call(0)
+        .then(function(result) {
+            console.log("token 0 address: ", result);
+            var tokenInst = CSAToken.at(result);
+            return tokenInst.owner.call();
+        })
+        .then (function(result) {
+            ownerAddress = result;
+            console.log("Owner address: ", result);
+            return tokenInst.balanceOf.call(ownerAddress);
+        })
+        .then(function(result) {
+            console.log("owners balance (before transfer): " + result);
+            assert.equal(result, 25, "Expected owner balance error");
+            return tokenInst.transfer(accounts[0], 1, {from: accounts[0]});
+        })
+        .then(function(result) {
+            console.log("result: ", result);
+        })
+
     });
 
 });
